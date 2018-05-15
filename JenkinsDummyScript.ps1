199d6aef4dedb3885432a828e6697e01e55add68
William Shiell <williamdshiell@gmail.com>
2018-05-15 08:27:35 -0500

    Adding Dummy Script

---
+++ JenkinsDummyScript.ps1
@@ -0,0 +1,15 @@
+﻿$Data = import-csv "C:\Users\willi_l9yp8fg\Desktop\Data.csv"
+
+foreach($user in $Data)
+{
+    $From  = $user.FromEmail
+    $To = $user.ToEmail
+    $Subject = $user.Subject 
+    $Body = $user.Body
+
+    Write-Output $From
+    Write-Output $To
+    Write-Output $Subject
+    Write-Output $Body
+    
+}
